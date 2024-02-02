5a3f251cb4be124a15ea84089e3ca11cbfc7f728
Developerss <developer.dev24@gmail.com>
2024-02-02 16:46:55 +0530

    intial commit

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-
+    name='oidc',
+    version='0.1',
+    packages=find_packages(),
     install_requires=[
-        'django'
+        'Django>=3.0',
+        'djangorestframework>=3.12',
     ]
 )
